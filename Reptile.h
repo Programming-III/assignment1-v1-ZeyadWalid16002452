5486d71633994d6996587b9fa675c063bfc804d4
ZeyadWalid16002452 <zeyadwalid11106@gmail.com>
2025-11-12 15:18:12 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,18 @@
-#ifndef REPTILE_H
-#define REPTILE_H
-
+#ifndef REPTILE_H_INCLUDED
+#define REPTILE_H_INCLUDED
+#include <iostream>
+using namespace std;
+#include <string>
 #include "Animal.h"
+private:
+    bool isVenomous;
+public:
+    Reptile();
+    Reptile(float wingSpan);
+    ~Reptile();
+    bool getIsVenomus();
+    void setIsVenoums();
 
-//define reptile class here
 
 
-
-
-
-
-
-
-
-#endif
+#endif // REPTILE_H_INCLUDED
